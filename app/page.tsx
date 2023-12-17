8f1f7b5ec516ff308cf460acc4fbd8df490f5af8
Marco Haber <marcoshaber99@gmail.com>
2023-12-17 21:51:15 +0200

    folder setup done

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <Button variant="destructive" size="lg">
-        click me
-      </Button>
+      <Button className="bg-emerald-500 text-white">Delete</Button>
     </div>
   );
 }
