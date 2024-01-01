5ed113671c7f8704674b54a8629b079cf11bd10a
Marco Haber <marcoshaber99@gmail.com>
2024-01-01 22:12:24 +0200

    next up set up some refs

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -11,7 +11,7 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
   if (isLoading) {
     return (
       <div className="h-full flex items-center justify-center">
-        <Spinner />
+        <Spinner size="lg" />
       </div>
     );
   }
