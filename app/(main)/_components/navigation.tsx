34bb22b370e95422cea2cfc6850b3bfee16ba857
Marco Haber <marcoshaber99@gmail.com>
2024-01-07 23:12:27 +0200

    user profile, dropdown, logout

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -6,6 +6,7 @@ import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 
 import { cn } from "@/lib/utils";
+import { UserItem } from "./user-item";
 
 export const Navigation = () => {
   const pathname = usePathname();
@@ -124,7 +125,7 @@ export const Navigation = () => {
           <ChevronsLeft className="h-6 w-6" />
         </div>
         <div>
-          <p>action items</p>
+          <UserItem />
         </div>
 
         <div className="mt-4">
